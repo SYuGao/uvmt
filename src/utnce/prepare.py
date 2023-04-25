864b2e1516d4463532ccf12dca421a502d6c970d
SYuGao <yuey1225@gmail.com>
2023-04-25 15:29:17 +0200

    clear functions and py documents

--- src/utnce/prepare.py
+++ src/utnce/prepare.py
@@ -50,7 +50,7 @@ def permutations(iterable, r=None):
             return 
         
         
-# Retrive data from OSM and get the geographic data of subway
+# Retrive data from OSM and get the geographic data of subway/tram
 def query_b(geoType,keyCol,**valConstraint):
     """
     This function builds an SQL query from the values passed to the retrieve() function.
@@ -123,12 +123,11 @@ def railway(osm_path):
     """
     Function to extract railway linestrings from OpenStreetMap
     Arguments:
-        *osm_path* : file path to the .osm.pbf file of the region 
-        for which we want to do the analysis.       
+        *osm_path* : file path to the .osm.pbf file of the region for which we want to do the analysis.       
     Returns:
-        *GeoDataFrame* : a geopandas GeoDataFrame with all unique land-use polygons.
+        *GeoDataFrame* : a geopandas GeoDataFrame with all railway linestrings.
     """ 
-    return retrieve(osm_path,'lines',['railway','service'])
+    return retrieve(osm_path,'lines',['railway','service'])     
 
 def subway_network(osm_path):
     """
@@ -157,7 +156,60 @@ def subway_network(osm_path):
     # Filter the DataFrame to include only subway railways
     subway = df_railway.loc[df_railway.railway == 'subway']
     
+    # Return the filtered DataFrame
     return subway
+
+def tram_network(osm_path):
+    """
+    Extracts tram network data from an OpenStreetMap file at the specified file path and returns it as a Pandas DataFrame.
+
+    Parameters:
+    osm_path (str): The file path of the OpenStreetMap file.
+
+    Returns:
+    Pandas DataFrame: A DataFrame containing tram network data extracted from the OpenStreetMap file.
+    """
+
+    # Extract railway data from the OpenStreetMap file
+    df_railway = railway(osm_path)
+    
+    # Filter the DataFrame to only contain tram network data
+    tram = df_railway.loc[df_railway.railway == 'tram']
+    
+    # Return the filtered DataFrame
+    return tram
+
+def tram_stations(osm_path):
+    """
+    Function to extract tram station nodes from OpenStreetMap.  
+    Arguments:
+        *osm_path* : file path to the .osm.pbf file of the region for which we want to do the analysis.       
+    Returns:
+        *GeoDataFrame* : a geopandas GeoDataFrame with all tram station nodes.
+    """   
+    
+    return (retrieve(osm_path,'points',['railway', 'name'],**{'railway':["='tram_stop'"]}))
+
+def routes(osm_path):
+    """
+    Extracts route data from an OpenStreetMap file at the specified file path and returns it as a Pandas DataFrame.
+
+    Parameters:
+    osm_path (str): The file path of the OpenStreetMap file.
+
+    Returns:
+    Pandas DataFrame: A DataFrame containing route data extracted from the OpenStreetMap file.
+    """
+
+    # Call the 'retrieve' function to extract route data from the OpenStreetMap file
+    # The 'retrieve' function takes three arguments:
+    # - The file path of the OpenStreetMap file
+    # - A list of geometry types to extract (in this case, only multilinestrings)
+    # - A list of tag keys to extract (in this case, 'route', 'name', and 'ref')
+    route_data = retrieve(osm_path, 'multilinestrings', ['route', 'name', 'ref'])
+
+    # Return the extracted route data as a Pandas DataFrame
+    return route_data
 
 
 # Pre-processing the geographic data of the subway network to obtain 'edges' and 'nodes'
@@ -255,3 +307,149 @@ def expand_edges(edges):
 
     # Return the expanded edges DataFrame
     return edges
+
+
+# Create s_e_tram_coordinates_pairs dataframe combining data of all stations and names of start-end stations    
+def s_e_tram_coordinates(stations, s_e_tram_station_name):
+    """
+    Given a GeoDataFrame `stations` containing information about tram stations and a DataFrame `s_e_tram_station_name` 
+    containing pairs of tram station names, returns a new DataFrame with columns `s_name`, `e_name`, `s_coordinates_x`, 
+    `s_coordinates_y`, `e_coordinates_x`, and `e_coordinates_y`.
+    
+    Parameters
+    ----------
+    stations : geopandas.GeoDataFrame
+        A GeoDataFrame containing information about tram stations, including the `geometry` column with the latitude 
+        and longitude coordinates of each station.
+    s_e_tram_station_name : pandas.DataFrame
+        A DataFrame containing pairs of tram station names, with columns `s_name` and `e_name`.
+    
+    Returns
+    -------
+    pandas.DataFrame
+        A new DataFrame with columns `s_name`, `e_name`, `s_coordinates_x`, `s_coordinates_y`, `e_coordinates_x`, and 
+        `e_coordinates_y`. `s_name` and `e_name` are the names of the starting and ending tram stations, respectively, 
+        and `s_coordinates_x`, `s_coordinates_y`, `e_coordinates_x`, and `e_coordinates_y` are the coordinates of the 
+        starting and ending tram stations.
+    """
+    
+    # Add columns for the x and y coordinates of each station
+    stations['geo_x'] = stations.geometry.x
+    stations['geo_y'] = stations.geometry.y
+    
+    # Drop duplicate stations and rename the two 'Centraal Station' stations as 'Centraal Station_A' and 'Centraal Station_B'
+    sorted_stations = stations.sort_values(by='geo_x')
+    stations_drop = sorted_stations[sorted_stations['name'] != 'Centraal Station'].drop_duplicates(subset=['name'], keep='first')
+    station_cetralB = sorted_stations[sorted_stations['name'] == 'Centraal Station'].drop_duplicates(subset=['name'], keep='first')
+    station_cetralB.iloc[0, 2] = 'Centraal Station_B'
+    station_cetralA = gpd.GeoDataFrame(sorted_stations[sorted_stations['name'] == 'Centraal Station'].iloc[-2]).T
+    station_cetralA.iloc[0, 2] = 'Centraal Station_A'
+    
+    # Create DataFrames for the stations and concatenate them into one DataFrame
+    df_stations_drop = pd.DataFrame(stations_drop)
+    df_station_cetralB = pd.DataFrame(station_cetralB)
+    df_station_cetralA = pd.DataFrame(station_cetralA)
+    tram_stations = pd.concat([df_stations_drop, df_station_cetralB, df_station_cetralA])
+    tram_stations
+    
+    # Create s_e_tram_coordinates DataFrame by compare station names 
+    df31 = pd.merge(s_e_tram_station_name, tram_stations, how='left', left_on='s_name', right_on='name')
+    s_e_tram_coordinates = pd.merge(df31, tram_stations, how='left', left_on='e_name', right_on='name')
+    s_e_tram_coordinates = s_e_tram_coordinates.loc[:,['s_name', 'e_name', 'geo_x_x', 'geo_y_x', 'geo_x_y', 'geo_y_y']]
+    s_e_tram_coordinates.columns = ['s_name', 'e_name', 's_coordinates_x', 's_coordinates_y', 'e_coordinates_x', 'e_coordinates_y']
+    
+    return s_e_tram_coordinates
+
+def s_e_coordinates_pairs(s_e_coordinates):
+    """
+    Given a Pandas DataFrame `s_e_coordinates` with columns `s_coordinates_x`, `s_coordinates_y`, `e_coordinates_x`, 
+    and `e_coordinates_y`, returns a new DataFrame with columns `s_coordinates` and `e_coordinates`.
+    
+    `s_coordinates` and `e_coordinates` are each a tuple of the form `(x, y)`, where `x` is the value in the 
+    `s_coordinates_x` or `e_coordinates_x` column, and `y` is the value in the `s_coordinates_y` or `e_coordinates_y` 
+    column.
+    
+    Parameters
+    ----------
+    s_e_coordinates : pandas.DataFrame
+        A DataFrame with columns `s_coordinates_x`, `s_coordinates_y`, `e_coordinates_x`, and `e_coordinates_y`.
+    
+    Returns
+    -------
+    pandas.DataFrame
+        A new DataFrame with columns `s_coordinates` and `e_coordinates`.
+    """
+    
+    # Create an empty DataFrame to hold the new columns
+    s_e_coordinates_pairs = pd.DataFrame()
+    
+    # Create the `s_coordinates` column by zipping the `s_coordinates_x` and `s_coordinates_y` columns
+    s_e_coordinates_pairs['s_coordinates'] = list(zip(s_e_coordinates.s_coordinates_x, s_e_coordinates.s_coordinates_y))
+    
+    # Create the `e_coordinates` column by zipping the `e_coordinates_x` and `e_coordinates_y` columns
+    s_e_coordinates_pairs['e_coordinates'] = list(zip(s_e_coordinates.e_coordinates_x, s_e_coordinates.e_coordinates_y))
+    
+    # Return the new DataFrame
+    return s_e_coordinates_pairs
+
+
+# Define nearest nodes with real_world coordinates of start and end points, obtain the nearest id pairs of nodes 
+def find_nearest_node(coordinate, nodes):
+    """
+    Find the nearest node to a given coordinate or geometry in a GeoDataFrame of nodes.
+
+    Args:
+    - coordinate: a tuple of longitude and latitude (in decimal degrees) or a PyGEOS geometry object representing the location to search from
+    - nodes: a GeoDataFrame containing nodes with a 'geometry' column representing their coordinates as PyGEOS Point objects
+    
+    Returns:
+    - the id value of the nearest node to the input coordinate or geometry
+
+    Example:
+    >>> import geopandas
+    >>> from shapely.geometry import Point
+    >>> from pygeos import Geometry
+    >>> nodes = geopandas.read_file('nodes.shp')
+    >>> coordinate = (-122.3, 47.6)
+    >>> find_nearest_node(coordinate, nodes)
+    1234
+
+    Note: This function requires the PyGEOS and STRtree libraries to be installed.
+    """
+    # Build an STRtree index of the nodes' geometries for efficient nearest-neighbor search
+    node_tree = pygeos.STRtree(nodes.geometry)
+   
+    # Find the nearest node to the input coordinate or geometry using the STRtree index
+    if isinstance(coordinate, tuple):
+        find_nearest = node_tree.nearest(pygeos.points(coordinate))
+    elif isinstance(coordinate, pygeos.lib.Geometry):
+        find_nearest = node_tree.nearest(coordinate)
+    
+    # Return the id value of the nearest node from the nodes GeoDataFrame
+    return int(nodes.iloc[find_nearest[1]]['id'])
+
+def id_pairs(coordinates_pairs, nodes):
+    """
+    Map the start and end coordinates to the nearest nodes in a transportation network.
+
+    Args:
+    - coordinates_pairs: a pandas DataFrame containing start and end coordinates in the network, with columns 's_coordinates', 'e_coordinates'
+    - nodes: a GeoDataFrame containing nodes in the network, with a 'geometry' column representing their coordinates as PyGEOS Point objects
+
+    Returns:
+    - a pandas DataFrame containing the nearest node IDs for each start and end coordinate pair, with columns 's_id', 'e_id'
+
+    """
+    # Initialize an empty DataFrame to store the start and end node IDs
+    id_pairs = pd.DataFrame(columns=['s_id','e_id'])
+    pairs_num = coordinates_pairs.shape[0]
+    
+    # For each start and end coordinate pair, find the nearest node in the network and store the node IDs in the DataFrame
+    for i in range(pairs_num):
+        s_coordinate = pygeos.points(coordinates_pairs.s_coordinates[i])
+        s_id = find_nearest_node(s_coordinate, nodes)
+        e_coordinate = pygeos.points(coordinates_pairs.e_coordinates[i])
+        e_id = find_nearest_node(e_coordinate, nodes)
+        id_pairs.loc[i] = [s_id,e_id]
+        
+    return id_pairs
--- src/utnce/routing.py
+++ src/utnce/routing.py
@@ -55,68 +55,6 @@ def create_ground_graph(edges, nodes):
     
     return G
 
-# Define nearest nodes with real_world coordinates of start and end points, obtain the nearest id pairs of nodes 
-def find_nearest_node(coordinate, nodes):
-    """
-    Find the nearest node to a given coordinate or geometry in a GeoDataFrame of nodes.
-
-    Args:
-    - coordinate: a tuple of longitude and latitude (in decimal degrees) or a PyGEOS geometry object representing the location to search from
-    - nodes: a GeoDataFrame containing nodes with a 'geometry' column representing their coordinates as PyGEOS Point objects
-    
-    Returns:
-    - the id value of the nearest node to the input coordinate or geometry
-
-    Example:
-    >>> import geopandas
-    >>> from shapely.geometry import Point
-    >>> from pygeos import Geometry
-    >>> nodes = geopandas.read_file('nodes.shp')
-    >>> coordinate = (-122.3, 47.6)
-    >>> find_nearest_node(coordinate, nodes)
-    1234
-
-    Note: This function requires the PyGEOS and STRtree libraries to be installed.
-    """
-    # Build an STRtree index of the nodes' geometries for efficient nearest-neighbor search
-    node_tree = pygeos.STRtree(nodes.geometry)
-   
-    # Find the nearest node to the input coordinate or geometry using the STRtree index
-    if isinstance(coordinate, tuple):
-        find_nearest = node_tree.nearest(pygeos.points(coordinate))
-    elif isinstance(coordinate, pygeos.lib.Geometry):
-        find_nearest = node_tree.nearest(coordinate)
-    
-    # Return the id value of the nearest node from the nodes GeoDataFrame
-    return int(nodes.iloc[find_nearest[1]]['id'])
-
-
-def id_pairs(coordinates_pairs, nodes):
-    """
-    Map the start and end coordinates to the nearest nodes in a transportation network.
-
-    Args:
-    - coordinates_pairs: a pandas DataFrame containing start and end coordinates in the network, with columns 's_coordinates', 'e_coordinates'
-    - nodes: a GeoDataFrame containing nodes in the network, with a 'geometry' column representing their coordinates as PyGEOS Point objects
-
-    Returns:
-    - a pandas DataFrame containing the nearest node IDs for each start and end coordinate pair, with columns 's_id', 'e_id'
-
-    """
-    # Initialize an empty DataFrame to store the start and end node IDs
-    id_pairs = pd.DataFrame(columns=['s_id','e_id'])
-    pairs_num = coordinates_pairs.shape[0]
-    
-    # For each start and end coordinate pair, find the nearest node in the network and store the node IDs in the DataFrame
-    for i in range(pairs_num):
-        s_coordinate = pygeos.points(coordinates_pairs.s_coordinates[i])
-        s_id = find_nearest_node(s_coordinate, nodes)
-        e_coordinate = pygeos.points(coordinates_pairs.e_coordinates[i])
-        e_id = find_nearest_node(e_coordinate, nodes)
-        id_pairs.loc[i] = [s_id,e_id]
-        
-    return id_pairs
-
 
 # Calculate the shorted path
 def shortest_path(G, start_point_id, end_point_id, edges, weight = "weight"):   # calculate the shortest path for one start_end_node id_pair
