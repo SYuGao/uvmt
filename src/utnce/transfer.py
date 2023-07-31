96b5d5b53db8ec489a7b3e6dba2fd70c5c3ac212
SYuGao <yuey1225@gmail.com>
2023-07-31 16:35:39 +0200

    transfer function_shortest_distance_time_plot

--- src/utnce/transfer.py
+++ src/utnce/transfer.py
@@ -6,6 +6,7 @@ import numpy
 from shapely.wkb import loads
 from shapely.geometry import Point
 from shapely.geometry import LineString
+from geopy.distance import geodesic
 import matplotlib.pyplot as plt
 import openpyxl
 import itertools
@@ -34,16 +35,22 @@ def add_ref_to_orderroutes_or_shortestpath_dict(order_route_dict, routes_df):
     return order_route_dict
 
 def create_tuple_column(df):
+    
     df['coordinate_value'] = list(zip(df['geo_x'], df['geo_y']))
+    
     return df
 
 def check_transfer(transfer):
+    
     if isinstance(transfer, tuple) and len(transfer) > 1 and len(set(transfer)) > 1:
+        
         return transfer
 
 def check_ref(ref):
+    
     if isinstance(ref, str) and len(ref) > 1 and not all(item is None for item in ref):
         ref = ref.replace(" ", "").replace(",", "")
+        
         return tuple(ref)
 
 def add_columns_to_nodes(order_route_dict, aggregation_functions, nodes):
@@ -67,3 +74,48 @@ def add_columns_to_edges(order_route_dict, aggregation_functions, edges):
     new_edges = pd.merge(edges, new_order_route_stations_df, on='geometry', how='outer')
 
     return new_edges
+
+def transfer_shortest_path(s_e_coordinates,new_nodes):
+    
+    start_end_points_coordinates_pairs = pd.DataFrame([s_e_coordinates])
+    start_end_points_coordinates_pairs = s_e_coordinates_pairs(start_end_points_coordinates_pairs)
+    start_end_nearest_id_pairs = id_pairs(start_end_points_coordinates_pairs, new_nodes)
+    start_point_id = start_end_nearest_id_pairs.iloc[0]['s_id']
+    end_point_id = start_end_nearest_id_pairs.iloc[0]['e_id']
+    
+    G = create_ground_graph(new_edges, new_nodes)
+    path_s_e, length_s_e, short_path_edges = shortest_path(G, start_point_id, end_point_id, new_edges, weight = "weight")
+
+    return path_s_e, length_s_e, short_path_edges,start_end_points_coordinates_pairs,start_end_nearest_id_pairs
+
+def walking_linear_distance(start_end_points_coordinates_pairs,start_end_nearest_id_pairs):
+    
+    start_point_tuple = start_end_points_coordinates_pairs.iloc[0]['s_coordinates']
+    end_point_tuple = start_end_points_coordinates_pairs.iloc[0]['e_coordinates']
+
+    start_nodes_df = pd.DataFrame(new_nodes.loc[new_nodes['id'] == start_end_nearest_id_pairs.iloc[0]['s_id']])
+    end_nodes_df = pd.DataFrame(new_nodes.loc[new_nodes['id'] == start_end_nearest_id_pairs.iloc[0]['e_id']])
+    start_nodes_tuple = (start_nodes_df.iloc[0]['geometry'].x, start_nodes_df.iloc[0]['geometry'].y)
+    end_nodes_tuple = (end_nodes_df.iloc[0]['geometry'].x, end_nodes_df.iloc[0]['geometry'].y)
+
+    distance_s_s_tuple = geodesic(start_point_tuple, start_nodes_tuple).meters
+    distance_e_e_tuple = geodesic(end_point_tuple, end_nodes_tuple).meters
+    
+    return distance_s_s_tuple,distance_e_e_tuple
+
+def transfer_using_time(short_path_edges,distance_s_s_tuple,distance_e_e_tuple):
+    
+    time_on_transport = short_path_edges['time'].sum()
+    speed_of_walking = 5000
+    time_of_walking = distance_s_s_tuple/speed_of_walking + distance_e_e_tuple/speed_of_walking
+    using_time_hour = time_on_sub + time_of_walking
+    using_time_minutes = round(using_time_hour * 60)
+    
+    return using_time_hour,using_time_minutes
+
+def plot_chosen_route(new_edges,short_path_edges):
+
+    fig, ax = plt.subplots(1, 1, figsize=(30, 20))
+
+    gpd.GeoDataFrame(new_edges.copy()).plot(ax=ax, color='gray', alpha=0.2)
+    gpd.GeoDataFrame(short_path_edges.copy()).plot(ax=ax, zorder=1, linewidth=(short_path_edges.count_weight) * 2, color='orange')
